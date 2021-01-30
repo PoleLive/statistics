f38482d32cec5f5c66288903eb1bf68b244c117e
庄健春 <zhuangjianchun@Jonathans-MacBook-Pro.local>
2021-01-30 09:39:41 +0800

    update

--- main.cpp
+++ main.cpp
@@ -5,7 +5,12 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QFile file("app.qss");//路径为应用程序所在目录开始
+#ifdef Q_OS_MAC
+    QString path = "/Users/zhuangjianchun/Project/statistics/bin/app.qss";
+#else
+    QString path = "app.qss";
+#endif
+    QFile file(path);//路径为应用程序所在目录开始
             file.open(QFile::ReadOnly);
             QTextStream file_text(&file);
             QString str_ret= file_text.readAll();
